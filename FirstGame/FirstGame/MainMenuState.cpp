3298a7f0aeedd130cc0f1052c5fb8feecfda87eb
Niko Kinnunen <nikokinnunen92@gmail.com>
2016-02-01 21:19:59 +0200

    menu still in the works.. some errors

--- FirstGame/FirstGame/MainMenuState.cpp
+++ FirstGame/FirstGame/MainMenuState.cpp
@@ -1,23 +1,166 @@
 #include "MainMenuState.h"
 
 
-MainMenuState::MainMenuState(GameApp* app) : GameState(app)
+MainMenuState::MainMenuState(GameApp* app) : GameState(app), m_app(app), m_map(0)
 {
+	// Tile size
+	vec2 tileSize(200, 104);
+
+	// Create new map, with width == tile, heigh == 32pixels/tile
+	m_map = new Map(tileSize.x, tileSize.y);
+
+	// Background layer
+	Layer* backgroundLayer = new Layer(m_map, "Background", 1.0f, true, false);
+
+	m_map->addLayer(Map::BACKGROUND0, backgroundLayer);
+
+	// Create new sprite GameObject from texture (background sprite) size is same than screen size.
+	GameObject* backgroundGameObject = createSpriteGameObject("meepobackground.png", 1280.0f, 720.0f, false);
+
+	// Add GameObject to background layer.
+	backgroundLayer->addGameObject(backgroundGameObject);
+
+	// Objects layer
+	Layer* objectsLayer = new Layer(m_map, "Objects", 1.0f, true, false);
+
+	m_map->addLayer(Map::MAPLAYER0, objectsLayer);
+
+	// Create new start button object, and clip it from texture postion <0,0> - <200, 52>, white color shall be transparent
+	GameObject* startButtonObject = createSpriteGameObject("buttons.png", tileSize.x, tileSize.y, 0, 0, 200, 52, true);
 	
+	// Add start button to level
+	objectsLayer->addGameObject(startButtonObject);
+
+	// Set start button position
+	startButtonObject->setPosition(vec2(1, 1));
+
+	// Create new start button object, and clip it from texture postion <0,52> - <200, 104>, white color shall be transparent
+	GameObject* exitButtonObjects = createSpriteGameObject("buttons.png", tileSize.x, tileSize.y, 0, 52, 400, 104, true);
+
+	// Add exit button to level
+	objectsLayer->addGameObject(exitButtonObjects);
+
+	// Set exit button position
+	exitButtonObjects->setPosition(vec2(2, 2));
+
+	// Setting object names
+	startButtonObject->setName("Start");
+	exitButtonObjects->setName("Exit");
 }
 
-
-MainMenuState::~MainMenuState()
+GameObject* MainMenuState::createSpriteGameObject(const std::string& bitmapFileName, float sizeX, float sizeY, bool isWhiteTransparentColor = false)
 {
+	// Load texture to be used as texture for sprite.
+	Texture* texture = new Texture(bitmapFileName.c_str());
+
+	// If user wants to create texture which white coros is treated as atransparent color.
+	if (isWhiteTransparentColor)
+	{
+		// Set white to transparent. Here color values are from 0 to 255 (RGB)
+		texture->setTransparentColor(255, 255, 255);
+	}
+
+	// Create new sprite GameObject from texture.
+	GameObject* gameObject = new GameObject(0, 0);
+	SpriteComponent* sprite = new SpriteComponent(gameObject, texture);
+
+	// Resize the sprite to be correct size
+	gameObject->setSize(sizeX, sizeY);
+
+	// Add sprite component to game object
+	gameObject->addComponent(sprite);
+	return gameObject;
 }
 
+GameObject* MainMenuState::createSpriteGameObject(const std::string& bitmapFileName, float sizeX, float sizeY, int clipStartX, int clipStartY, int clipSizeX, int clipSizeY, bool isWhiteTransparentColor = false)
+{
+	// Load texture to be used as texture for sprite.
+	Texture* texture = new Texture(bitmapFileName.c_str());
+
+	// If user wants to create texture which white coros is treated as atransparent color.
+	if (isWhiteTransparentColor)
+	{
+		// Set white to transparent. Here color values are from 0 to 255 (RGB)
+		texture->setTransparentColor(255, 255, 255);
+	}
+
+	// Create new sprite GameObject from texture.
+	GameObject* gameObject = new GameObject(0, 0);
+	SpriteComponent* sprite = new SpriteComponent(gameObject, texture);
+
+	// Resize the sprite to be correct size
+	gameObject->setSize(sizeX, sizeY);
+
+	// Specify clip area by start point and size in pixels
+	Sprite::PixelClip clip;
+	clip.topLeft.x = clipStartX;
+	clip.topLeft.y = clipStartY;
+	clip.clipSize.x = clipSizeX;
+	clip.clipSize.y = clipSizeY;
+
+	// Set pixel clip for sprite
+	sprite->getSprite()->setClip(float(texture->getWidth()), float(texture->getHeight()), clip);
+
+	// Add sprite component to game object
+	gameObject->addComponent(sprite);
+	return gameObject;
+}
 
 bool MainMenuState::update(ESContext* ctx, float deltaTime)
 {
+	//m_map->getCamera()->setPosition(5, 0);
+
+	// Read mouse values
+	float mouseX = float(getMouseAxisX());
+	float mouseY = float(getMouseAxisY());
+
+	// Convert mouse coordinates to map coordinates.
+	vec2 mouseInMapCoordinates = m_map->screenToMapCoordinates(mouseX, mouseY);
+
+	GameObject* pickedObject = m_map->getLayer("Objects")->pick(mouseInMapCoordinates);
+
+	std::string start = "Start";
+	std::string exit = "Exit";
+
+	if (pickedObject != nullptr)
+	{
+		std::string test1 = pickedObject->getName();
+		if (test1.compare(start) == 0)
+		{
+			esLogMessage("Object %s picked!", pickedObject->getName().c_str());
+
+		}
+		else if (test1.compare(exit) == 0)
+		{
+			esLogMessage("Object not picked!");
+		}
+	}
+	
+
+
+	// Update map. this will update all GameObjects inside a map layers.
+	m_map->update(deltaTime);
+
 	return true;
 }
 
 void MainMenuState::render(ESContext* ctx)
 {
-	
+	// Set OpenGL clear color (dark gray)
+	glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+
+	// Clear the color buffer
+	glClear(GL_COLOR_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+
+	// Set screen size to camera.
+	m_map->getCamera()->setScreenSize(ctx->width, ctx->height, 720, 1280.0f / 720.0f);
+
+	// Render map and all of its layers containing GameObjects to screen.
+	m_map->render();
+}
+
+MainMenuState::~MainMenuState()
+{
+	// Delete map
+	delete m_map;
 }
--- FirstGame/FirstGame/MainMenuState.h
+++ FirstGame/FirstGame/MainMenuState.h
@@ -1,7 +1,13 @@
 #pragma once
+#include "es_util.h"
 #include "GameState.h"
 #include "Map.h"
 #include "Input.h"
+#include "TileComponent.h"
+#include "SpriteComponent.h"
+#include "Layer.h"
+#include "Camera.h"
+#include <iostream>
 
 class MainMenuState : public GameState
 {
@@ -11,6 +17,9 @@ public:
 
 	virtual bool update(ESContext* ctx, float deltaTime);
 	virtual void render(ESContext* ctx);
+
+	GameObject* createSpriteGameObject(const std::string& bitmapFileName, float sizeX, float sizeY, bool isWhiteTransparentColor);
+	GameObject* createSpriteGameObject(const std::string& bitmapFileName, float sizeX, float sizeY, int clipStartX, int clipStartY, int clipSizeX, int clipSizeY, bool isWhiteTransparentColor);
 
 private:
 	GameApp* m_app;
