0340bf1c738b1fd703a39a8682099c35a2840f20
Niko Kinnunen <nikokinnunen92@gmail.com>
2016-02-07 18:08:27 +0200

    added level.tmx, gamerunningstate now renders tilemap for the first level

--- FirstGame/FirstGame/GameRunningState.h
+++ FirstGame/FirstGame/GameRunningState.h
@@ -10,17 +10,13 @@ public:
 
 	virtual bool update(ESContext* ctx, float deltaTime);
 	virtual void render(ESContext* ctx);
+	float getZoom();
+	void setZoom(float newZoom);
 
 private:
-	float m_totalTime = 0;
-	float m_count = 0;
-	Ref<Map> m_map;
-	GameApp* m_app;
-	Ref<Texture> m_texture = 0;
-	Ref<Text> m_text = 0;
-	Ref<Texture> m_fontTexture = 0;
-	Ref<Sprite> m_sprite = 0;
-	Ref<SpriteBatchGroup> m_batch = 0;
-	Ref<SpriteSheet> m_font = 0;
+	float deltaTime = 0;
+	TmxMap* map = 0;
+	ComponentFactory* componentFactory = 0;
+	float zoom = 1.0f;
 };
 
